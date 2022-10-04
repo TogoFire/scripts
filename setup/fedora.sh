fed0aca17180d02c5862dfe54461aa7abbf10d04
TogoFire <togofire@mailfence.com>
2022-10-04 18:29:55 -0300

    setup: Install the Development and build tools on Fedora
    Change-Id: ec76d71360e34f05ba783fc5ba1c57ed0f91c06b
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- setup/fedora.sh
+++ setup/fedora.sh
@@ -50,6 +50,9 @@ sudo dnf install \
     vboot-utils \
     vim
 
+# Development and build tools
+sudo dnf groupinstall "Development Tools" "Development Libraries"
+
 # The package libncurses5 is not available, so we need to hack our way by symlinking the required library.
 sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
 sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libtinfo.so.5
